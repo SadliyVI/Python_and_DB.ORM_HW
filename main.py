725b7351c264d7c738612f51fae3b1be0e10b6a6
V.Sadliy <sadliy@mail.ru>
2025-01-14 19:26:25 +0300

    Added loading information from json file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+from datetime import datetime
 import configparser
 import psycopg2
 from psycopg2 import Error
@@ -22,11 +24,35 @@ if __name__ == "__main__":
     Session = sessionmaker(bind = engine)
     session = Session()
 
+    with open('fixtures/test_data.json') as f:
+        data = json.load(f)
 
+    for item in data:
+        model = item['model']
+        pk = item['pk']
+        fields = item['fields']
 
-
-
-
+        if model == 'publisher':
+            publisher = Publisher(id = pk, name = fields['name'])
+            session.add(publisher)
+        elif model == 'book':
+            book = Book(id = pk, title = fields['title'],
+                        id_publisher = fields['id_publisher'])
+            session.add(book)
+        elif model == 'shop':
+            shop = Shop(id = pk, name = fields['name'])
+            session.add(shop)
+        elif model == 'stock':
+            stock = Stock(id = pk, id_book = fields['id_book'],
+                          id_shop = fields['id_shop'], count = fields['count'])
+            session.add(stock)
+        elif model == 'sale':
+            date_sale = datetime.strptime(fields['date_sale'],
+                                          '%Y-%m-%dT%H:%M:%S.%fZ').date()
+            sale = Sale(id = pk, price = fields['price'], date_sale =
+            date_sale, id_stock = fields['id_stock'], count = fields['count'])
+            session.add(sale)
+    session.commit()
 
 
 
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 import sqlalchemy as sq
+from sqlalchemy import CheckConstraint
 from sqlalchemy.orm import declarative_base, relationship
+from datetime import date
 
 
 Base = declarative_base()
@@ -7,46 +9,54 @@ Base = declarative_base()
 class Publisher(Base):
     __tablename__ = 'publishers'
 
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(255), unique=True)
-    books = relationship('Book', backref='publisher')
+    id = sq.Column(sq.Integer, primary_key = True)
+    name = sq.Column(sq.String(length = 255), unique = True, nullable = False)
+    books = relationship('Book', backref = 'publisher')
 
 class Book(Base):
     __tablename__ = 'books'
 
-    id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String(255), unique=True)
-    author = sq.Column(sq.String(255))
+    id = sq.Column(sq.Integer, primary_key = True)
+    title = sq.Column(sq.String(length = 255), unique = True, nullable = False)
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publishers.id'),
-                             nullable=False)
-    stocks = relationship('Stock', backref='book')
+                             nullable = False)
+    stocks = relationship('Stock', backref = 'book')
 
 
 class Stock(Base):
     __tablename__ ='stocks'
 
-    id = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key = True)
     id_book = sq.Column(sq.Integer, sq.ForeignKey('books.id'))
     id_shop = sq.Column(sq.Integer, sq.ForeignKey('shops.id'))
     count = sq.Column(sq.Integer)
-    sales = relationship('Shop', backref = 'stock')
-    shop = relationship('Shop', backref='stocks')
+    __table_args__ = (CheckConstraint('count >= 0',
+                                       name='check_count_non_negative'),)
+    shop = relationship('Shop', backref = 'stocks')
+    sales = relationship('Sale', backref = 'stock')
+
 
 class Shop(Base):
     __tablename__ ='shops'
 
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(255))
+    id = sq.Column(sq.Integer, primary_key = True)
+    name = sq.Column(sq.String(length = 255), nullable = False)
 
 
 class Sale(Base):
     __tablename__ ='sales'
 
-    id = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key = True)
     price = sq.Column(sq.Float)
     date_sale = sq.Column(sq.Date)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey('stocks.id'))
     count = sq.Column(sq.Integer)
+    __table_args__ = (
+                      CheckConstraint('count >= 0',
+                                      name='check_count_non_negative'),
+                      CheckConstraint('price >= 0',
+                                      name='check_price_non_negative')
+    )
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
